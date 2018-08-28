f5a5480f4026bb2896cecca8f1557ac840b3c6fe
github_OriaborEzekiel <oriaborezekiel4success@gmail.com>
2018-08-28 12:10:20 -0700

    add migration for db

---
+++ database/migrations/2018_08_28_181922_create_spots_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSpotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spots', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('address');
+            $table->integer('user_id');
+            $table->integer('location_id');
+            $table->string('phone_no');
+            $table->string('avatar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spots');
+    }
+}
