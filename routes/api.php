5ee1fa910c9f9b1966564d62d72bec3a5eb17717
Awujoola <danielawujoola1@gmail.com>
2018-11-16 16:25:29 +0100

    spot api for spots and view

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/spots', 'SpotController@index');
+Route::get('/spots/{id}', 'SpotController@show');
