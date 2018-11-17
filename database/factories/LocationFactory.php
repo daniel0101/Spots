6275eca34811af9cac11fe7f45ea1dab6ceda7e4
Awujoola <danielawujoola1@gmail.com>
2018-11-17 12:49:56 +0100

    Refactor Factories for Location and User

--- database/factories/LocationFactory.php
+++ database/factories/LocationFactory.php
@@ -4,7 +4,6 @@ use Faker\Generator as Faker;
 use App\Location;
 $factory->define(Location::class, function (Faker $faker) {
     return [
-        'id'=>$faker->unique()->randomDigit,
         'name'=>$faker->city
     ];
 });
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'lastname' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'token' => $faker->asciify('********************'),
+        'token' => str_random(32),
         'is_verified' => rand(0,1),
         'type' => $faker->word,
         'remember_token' => str_random(10),
