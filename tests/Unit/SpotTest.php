f3efafabad2f7b7e19028a5a2a86e09b94a9e4e7
Awujoola <danielawujoola1@gmail.com>
2018-12-01 23:45:01 +0100

    Adds more unit tests and feature tests

--- tests/Unit/SpotTest.php
+++ tests/Unit/SpotTest.php
@@ -12,15 +12,18 @@ class SpotTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public function setUp(){
+        parent::setUp();
+        $this->spot = factory('App\Spot')->create();
+    }
+    
     /** @test */
     public function a_spot_belongs_to_a_user(){
-        $spot = factory('App\Spot')->create();
-        $this->AssertInstanceOf(User::class,$spot->user);
+        $this->AssertInstanceOf(User::class,$this->spot->user);
     }
 
     /** @test */
     public function a_spot_has_a_location(){
-        $spot = factory('App\Spot')->create();
-        $this->AssertInstanceOf('App\Location',$spot->location);
+        $this->AssertInstanceOf('App\Location',$this->spot->location);
     }
 }
