337567d17c592141759398282b0a8713f03ae8ab
Awujoola <danielawujoola1@gmail.com>
2019-02-06 21:44:46 +0100

    adds a default value to is_verified field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('token');
-            $table->boolean('is_verified');
+            $table->boolean('is_verified')->default(false);
             $table->string('type');
             $table->rememberToken();
             $table->timestamps();
