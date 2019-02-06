0a0fdddabe308f9c016f56fccc1e8382f18700eb
Awujoola <danielawujoola1@gmail.com>
2019-02-06 21:39:18 +0100

    new api routes added

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,32 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Route::group([
+
+//     'middleware' => 'api',
+//     'prefix' => 'auth'
+
+// ], function ($router) {
+
+//     Route::post('login', 'AuthController@login');
+//     Route::post('logout', 'AuthController@logout');
+//     Route::post('refresh', 'AuthController@refresh');
+//     Route::post('/user', 'AuthController@getAuthenticatedUser');
+
+// });
+
+Route::middleware(['auth:api'])->group(function () {    
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('/user', 'AuthController@getAuthenticatedUser');
+});
+
+Route::post('login', 'AuthController@login');
+
 Route::get('/spots', 'SpotController@index');
 Route::get('/spot/{id}', 'SpotController@show');
 Route::get('/spot/{id}/user', 'SpotController@user');
 Route::get('/spot/{id}/location', 'SpotController@location');
 
-Route::post('/spot/{id}', 'SpotController@store');
+Route::post('/spot', 'SpotController@store')->middleware('auth:api');
 
